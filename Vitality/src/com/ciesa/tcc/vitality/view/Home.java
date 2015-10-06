8ad6604a1b02867071e1aa6be8de8ed014dc7183
Jorenilson Lopes <jorenilsonlopes@gmail.com>
2015-10-06 10:02:25 -0400

    Implementação das Views da tela principal da aplicação.

---
+++ Vitality/src/com/ciesa/tcc/vitality/view/Home.java
@@ -0,0 +1,17 @@
+package com.ciesa.tcc.vitality.view;
+
+import com.ciesa.tcc.vitality.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Home extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home);
+	}
+
+}
